7d6a630a2046dd8879a40ce56dbca52d2d8993cc
Gustavo dos Santos <gs2096079@gmail.com>
2026-08-24 22:11:02 -0300

    study: pratica variaveis e saida de dados

---
+++ 01-logica/01-saida-de-dados/SaidaDeDados.java
@@ -0,0 +1,21 @@
+public class SaidaDeDados {
+    public static void main(String[] args) {
+        //Variaveis
+        //calculos
+        //decisoes
+        //repeticoes
+        //A logica do exercicio
+        //saida de dados
+
+        String produto1= "Computador";
+        String produto2= "TV";
+        Double preco1= 2100.5;
+        Double preco2= 1830.0;
+        Integer idade = 30;
+        Integer codigo = 5291;
+        String genero = "F";
+
+    System.out.println("Produtos:\n " + "O produto " + produto1 + " Custa " + preco1 + "\n " + "O produto " + produto2 + " Custa " + preco2 + "\n\n " + " Codigo: " + codigo + "\n\n " + " Dados da pessoa : Genero : " + genero + " E idade : " + idade);
+    
+    }
+}
